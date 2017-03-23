a73a9d5a13e279e9399a875d32d47d8425849277
Mincong HUANG <mincong.h@gmail.com>
2017-03-23 09:21:12 +0100

    add more URLs for Stack Overflow helper

--- action_stackoverflow/background.js
+++ action_stackoverflow/background.js
@@ -3,8 +3,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   var urls = [
     'https://stackoverflow.com/questions/tagged/java+date',
     'https://stackoverflow.com/questions/tagged/java%20collections',
+    'https://stackoverflow.com/questions/tagged/java+list',
+    'https://stackoverflow.com/questions/tagged/java%20dictionary',
     'https://stackoverflow.com/questions/tagged/java%20arrays',
-    'https://stackoverflow.com/questions/tagged/java-8'
+    'https://stackoverflow.com/questions/tagged/java-8',
+    'https://stackoverflow.com/questions/tagged/git'
   ];
   urls.forEach(function(url) {
     window.open(url);
