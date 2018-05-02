72a503e7eb2f974230b11afed6599d661a633cb7
Mincong HUANG <mhuang@nuxeo.com>
2018-05-02 10:58:08 +0200

    Add more combination

--- action-stackoverflow/background.js
+++ action-stackoverflow/background.js
@@ -1,12 +1,15 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   var urls = [
-    'https://stackoverflow.com/questions/tagged/java+date',
-    'https://stackoverflow.com/questions/tagged/java%20collections',
-    'https://stackoverflow.com/questions/tagged/java+list',
-    'https://stackoverflow.com/questions/tagged/java%20dictionary',
-    'https://stackoverflow.com/questions/tagged/java%20arrays',
+    'https://stackoverflow.com/questions/tagged/java%20selenium',
+    'https://stackoverflow.com/questions/tagged/java%20xml',
+    'https://stackoverflow.com/questions/tagged/java%20macos',
+    'https://stackoverflow.com/questions/tagged/java%20jersey',
+    'https://stackoverflow.com/questions/tagged/java%20telegram',
     'https://stackoverflow.com/questions/tagged/java-8',
+    'https://stackoverflow.com/questions/tagged/jgit',
+    'https://stackoverflow.com/questions/tagged/bash',
+    'https://stackoverflow.com/questions/tagged/maven',
     'https://stackoverflow.com/questions/tagged/git'
   ];
   urls.forEach(function(url) {
